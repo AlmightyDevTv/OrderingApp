f039f09de0f180d44578b9b64d321259a4f19e02
Smith <1069589@bcpss.org>
2018-11-20 09:52:42 -0500

    i created a textview for the name of my store and buttons to show the drinks and food on the menu

--- app/src/main/java/com/patterson/dsmith/orderingapp/MainActivity.java
+++ app/src/main/java/com/patterson/dsmith/orderingapp/MainActivity.java
@@ -2,12 +2,25 @@ package com.patterson.dsmith.orderingapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Button softDrinks;
+    Button Hamburger;
+    TextView Almightyskitchen;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Almightyskitchen = (TextView) findViewById(R.id.Almightyskithchen);
+        Hamburger = (Button) findViewById(R.id.button1);
+        softDrinks = (Button) findViewById(R.id.button2);
+
+
+
+
     }
 }
