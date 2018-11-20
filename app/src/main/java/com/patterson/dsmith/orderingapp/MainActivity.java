668c995b58ff3fc345108b7da9189771f82c6150
Smith <1069589@bcpss.org>
2018-11-20 14:23:35 -0500

    i made a toast appear when i click the buttons

--- app/src/main/java/com/patterson/dsmith/orderingapp/MainActivity.java
+++ app/src/main/java/com/patterson/dsmith/orderingapp/MainActivity.java
@@ -2,25 +2,40 @@ package com.patterson.dsmith.orderingapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Button softDrinks;
     Button Hamburger;
     TextView Almightyskitchen;
-
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Almightyskitchen = (TextView) findViewById(R.id.Almightyskithchen);
-        Hamburger = (Button) findViewById(R.id.button1);
-        softDrinks = (Button) findViewById(R.id.button2);
+        Almightyskitchen = (TextView) findViewById(R.id.storename);
+        Hamburger = (Button) findViewById(R.id.buttonf1);
+        softDrinks = (Button) findViewById(R.id.buttond2);
 
 
+        Hamburger.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"typeofburger", Toast.LENGTH_SHORT).show();
+            }
+        });
 
+        softDrinks.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"typeofdrink",Toast.LENGTH_SHORT).show();
+
+            }
+        });
 
     }
 }
